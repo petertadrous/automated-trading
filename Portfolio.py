f9f5d7d3e9ec18a35967a7ecb8f605011d73a1e5
Peter Tadrous <petertadrous@gmail.com>
2021-08-03 15:34:53 -0400

    Added project to git repository.

---
+++ Portfolio.py
@@ -0,0 +1,101 @@
+import pandas as pd
+import numpy as np
+import pandas_datareader as pdr
+import matplotlib.pyplot as plt
+from multiprocessing import Pool
+import time
+from TradingSystem import TradingSystem
+
+# I was getting a weird error with numpy and plotting, this next line seemed to fix.
+pd.plotting.register_matplotlib_converters()
+
+# Portfolio class, takes a list of tickers, a start date, end date, max short, and max long to create a portfolio
+class Portfolio(object):
+    def __init__(self,tlist,start,end,max_short,max_long):
+        self.ticker_list = tlist
+        self.start = start
+        self.end = end
+        self.max_short = max_short + 1
+        self.max_long = max_long + 1
+        self.portfolio_list = []
+        self.trading_systems = {}
+        print('Created portfolio.\n{}\n\n'.format(tlist))
+    
+    # Calls find_best_moving_averages() and saves results
+    def trade(self):
+        for ticker in self.ticker_list:
+            self.trading_systems[ticker], ts_res = self.find_best_moving_averages(ticker)
+            self.portfolio_list.append(ts_res)
+    
+    # Takes a multiprocessing approach of previous function for better performance
+    def mp_trade(self):
+        print('Beginning multiprocessing.')
+        try:
+            pool = Pool()
+            self.ts_list = []
+            ts_res = pool.map(self.find_best_moving_averages, self.ticker_list)
+            for res in ts_res:
+                self.trading_systems[res[0].symbol] = res[0]
+                self.portfolio_list.append(res[1])
+        finally:
+            pool.close()
+            pool.join()
+    
+    # Programatically calculates each moving average in given winddow and finds optimal short and long
+    def find_best_moving_averages(self,ticker):
+        ts_ret = 0
+        tr_ret = 0
+        optimal_short = 0
+        optimal_long = 0
+        ts_perf = 0
+        print('Creating instance of trading system for {}.'.format(ticker))
+        ts = TradingSystem(ticker,self.start,self.end,optimal_short,optimal_long)
+        for short in range(2,self.max_short):
+            for long in range(short+3,self.max_long):
+                ts.short = short
+                ts.long = long
+                ts.trade()
+                temp_perf = ts.get_performance()
+                if temp_perf > ts_perf:
+                    ts_ret, tr_ret = ts.get_results()
+                    ts_perf = temp_perf
+                    optimal_short = short
+                    optimal_long = long
+        print('Found optimal moving averages for {}.\tShort: {}, Long: {}'.format(ticker,
+                                                                                    optimal_short,
+                                                                                    optimal_long))
+        ts.short = optimal_short
+        ts.long = optimal_long
+        ts.trade()
+        return ts, [ticker,self.start,self.end,optimal_short,optimal_long,ts_ret,tr_ret,ts_perf]
+    
+    # Creates a dataframe with the results
+    def create_portfolio(self):
+        self.portfolio_results_df = pd.DataFrame(self.portfolio_list, columns = ['Ticker',
+                                                                                 'Start',
+                                                                                 'End',
+                                                                                 'Short',
+                                                                                 'Long',
+                                                                                 'TS_Return',
+                                                                                 'Trad_Return',
+                                                                                 'Performance'])
+    
+    # Prints the results of the portfolio, with the option of displaying the graphs of each stock and ma's
+    def print_results(self,display_plots):
+        if display_plots:
+            for ticker in self.ticker_list:
+                self.trading_systems[ticker].plot_ma()
+                self.trading_systems[ticker].print_results()
+                print('\n{}\n'.format('*'*75))
+        print('\n')
+        performance_value = self.portfolio_results_df.Performance.mean()
+        print(performance_value)
+        if performance_value > 1:
+            print('On average, the trading system portfolio outperformed traditional portfolio by:{:>10.2%}'.format(performance_value-1))
+        elif performance_value < 1:
+            p_val = 1/performance_value
+            print('On average, traditional portfolio outperformed the trading system portfolio by:{:>10.2%}'.format(p_val-1))
+        else:
+            print('The trading system portfolio performance was equivalent to traditional portfolio.')
+        print(self.portfolio_results_df)
+        
---
+++ main.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+import pandas_datareader as pdr
+import matplotlib.pyplot as plt
+from multiprocessing import Pool
+import time
+from Portfolio import Portfolio
+from TradingSystem import TradingSystem
+import TRADECONFIG
+
+# I was getting a weird error with numpy and plotting, this next line seemed to fix.
+pd.plotting.register_matplotlib_converters()
+
+def main():
+    start_time = time.time()
+
+    tlist = TRADECONFIG.TICKER_LIST
+    start = TRADECONFIG.START_DATE
+    end = TRADECONFIG.END_DATE
+    max_short = TRADECONFIG.MAX_SHORT
+    max_long = TRADECONFIG.MAX_LONG
+    display_individual_plots = TRADECONFIG.DISPLAY_INDIVIDUAL_PLOTS
+
+    pf = Portfolio(tlist,start,end,max_short,max_long)
+    # Can use pf.trade() or multiprocessing computation with pf.mp_trade().
+    # mp_trade() doesn't work on windows because the spawn method is 'spawn' not 'fork'
+    pf.mp_trade()
+    pf.create_portfolio()
+    pf.print_results(display_individual_plots)
+
+    end_time = time.time()
+    run_time = end_time - start_time
+    print('Portfolio took {:>10.2} minutes to run.'.format(run_time/60))
+
+if __name__ == "__main__":
+    main()
